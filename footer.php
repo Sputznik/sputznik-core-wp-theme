e38409b063f48fafb70c01418b08e88dca667260
Samuel Thomas <samvthom16@gmail.com>
2017-11-14 15:25:03 +0530

    Include footer in the wrapper

--- footer.php
+++ footer.php
@@ -1,20 +1,25 @@
 					</div>
 				</div>
+				
+				<footer class="container-fluid">
+					<div class="row">
+						<div class="col-md-12 clearfix">
+							<span class="pull-left" style="margin-right:10px;"><a href="https://www.facebook.com/Sputznik" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a></span>
+							<span class="pull-left" style="margin-right:10px;"><a href="https://twitter.com/sputznikweb" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a></span>
+							<span class="pull-left"><a href="https://www.linkedin.com/in/guneetnarula" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a></span>
+							<span class="pull-right"><h4>Web Exploration.</h4></span>
+						</div>
+					</div>
+				</footer>
+				
+				
+				
 			</div>
 			<!-- /#page-content-wrapper -->
 
 		</div>
 		<!-- /#wrapper -->
-		<footer class="container-fluid">
-			<div class="row">
-				<div class="col-md-12 clearfix">
-					<span class="pull-left" style="margin-right:10px;"><a href="https://www.facebook.com/Sputznik" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a></span>
-					<span class="pull-left" style="margin-right:10px;"><a href="https://twitter.com/sputznikweb" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a></span>
-					<span class="pull-left"><a href="https://www.linkedin.com/in/guneetnarula" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a></span>
-					<span class="pull-right"><h4>Web Exploration.</h4></span>
-				</div>
-			</div>
-		</footer>
+		
 		<?php wp_footer();?>
 	</body>
 </html>
