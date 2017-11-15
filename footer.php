a6c18c01744c5fce9505e159431e09c64a258c9f
Samuel Thomas <samvthom16@gmail.com>
2017-11-15 10:03:45 +0530

    refactor footer

--- footer.php
+++ footer.php
@@ -4,10 +4,12 @@
 				<footer class="container-fluid">
 					<div class="row">
 						<div class="col-md-12 clearfix">
-							<span class="pull-left" style="margin-right:10px;"><a href="https://www.facebook.com/Sputznik" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a></span>
-							<span class="pull-left" style="margin-right:10px;"><a href="https://twitter.com/sputznikweb" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a></span>
-							<span class="pull-left"><a href="https://www.linkedin.com/in/guneetnarula" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a></span>
-							<span class="pull-right"><h4>Web Exploration.</h4></span>
+							<ul class="list-inline">
+								<li><a href="https://www.facebook.com/Sputznik" target="_blank"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+								<li><a href="https://twitter.com/sputznikweb" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+								<li><a href="https://www.linkedin.com/in/guneetnarula" target="_blank"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+								<li class="pull-right"><h4>Web Exploration.</h4></li>
+							</ul>
 						</div>
 					</div>
 				</footer>
