b5e8c00273cd8a29fd2854d990a408a9eaa15980
Jay Vardhan <vardhan.jay@gmail.com>
2018-09-03 18:28:27 +0530

    added post navigation link in single.php

--- single.php
+++ single.php
@@ -4,6 +4,12 @@
 			<div class="col-lg-8 col-lg-offset-2">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<?php the_content(); ?>
+					<div class="post-nav">
+						<span class="pull-left"><?php previous_post_link("&laquo; %link", "Previous"); ?></span>
+						<span class="pull-right">
+					    	<?php next_post_link("%link &raquo;", "Next"); ?>
+					    </span>
+					</div>
 				<?php endwhile; endif; ?>
 			</div>
 		</div>
