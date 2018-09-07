8b6d0f3e22745b311f52f9a6950b9ca6cd4f4247
Samuel Thomas <samvthom16@gmail.com>
2018-09-07 18:48:22 +0530

    Smooth Scroll

--- js/script.js
+++ js/script.js
@@ -1,34 +1,56 @@
+$.fn.smooth_scroll = function( options ) {
+
+	return this.each(function() {
+		   
+		var $el = $(this);
+		
+		// CHECK IF TARGET EXISTS WITHIN THE BODY
+		$el.isTargetValid = function(){
+			
+			var $target = $el.getTarget();
+			
+			if( $target.length ) {
+				return true;
+			}
+			
+			return false;
+			
+		};
+		
+		// PARSE URL - HELPER FUNCTION
+		$el.parseURL = function( url ){
+			var a = document.createElement('a');
+			a.href = url;
+			return a;
+		};
+		
+		// RETURN TARGET ELEMENT
+		$el.getTarget = function(){
+			var hash = $el.parseURL( $el.attr( 'href' ) ).hash;
+			return $( hash );
+		};
+		
+		
+		$el.on( 'click', function( event ) {
+			event.preventDefault();
+			if( $el.isTargetValid() ){
+				
+				$('.modal').modal('hide');
+				
+				$('html, body').stop().animate({
+					scrollTop: $el.getTarget().offset().top
+				}, 1000);
+				
+			}
+			
+		});
+			
+	});
+};
+
 $(document).ready(function () {
 	
+	$('a[href]').smooth_scroll();
 	
 	
-	var trigger = $('.hamburger'),
-		overlay = $('.overlay'),
-		isClosed = false;
-
-    trigger.click(function () {
-      hamburger_cross();      
-    });
-
-    function hamburger_cross() {
-
-		if (isClosed == true) {          
-			overlay.hide();
-			trigger.removeClass('is-open');
-			trigger.addClass('is-closed');
-			isClosed = false;
-		} else {   
-			overlay.show();
-			trigger.removeClass('is-closed');
-			trigger.addClass('is-open');
-			isClosed = true;
-		}
-	}
-  
-	$('[data-toggle="offcanvas"]').click(function () {
-		
-		
-		
-		$('#wrapper').toggleClass('toggled');
-	});  
 });
