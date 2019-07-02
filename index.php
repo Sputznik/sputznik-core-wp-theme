d11f2579fc1f5ae3f5128ce00d383c9192a0c6ed
unknown <samvthom16@gmail.com>
2019-07-02 15:06:52 +0530

    Page template added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php get_header();?>	
+<?php get_header();?>
 	<div id="content" class="container">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<?php the_content('Read the rest of this entry »'); ?>
 		<?php endwhile; endif; ?>
 	</div>
-<?php get_footer();?>				
+<?php get_footer();?>
