eca85e892bb1baf4947d914324e1a29ac62a74ac
unknown <samvthom16@gmail.com>
2019-07-12 09:56:23 +0530

    Add to cart in main menu - testing

--- functions.php
+++ functions.php
@@ -193,14 +193,15 @@
 	}, 10, 2 );
 
 	// ENABLE CART ICON ALONG WITH THE PRIMARY MENU
-	if ( class_exists( 'WooCommerce' ) ) {
+	//if ( class_exists( 'WooCommerce' ) ) {
 		add_filter( 'wp_nav_menu_items', function($items, $args){
+			//get_permalink( wc_get_page_id( 'cart' ) )
 			if( $args->theme_location == 'primary' ){
-		        $items .= '<li class="sp_cart_item"><a href="'.get_permalink( wc_get_page_id( 'cart' ) ).'"><i class="fa fa-cart"></i></a></li>';
+		        $items .= '<li class="sp_cart_item"><a href=""><i class="fa fa-cart"></i></a></li>';
 		    }
 			return $items;
 		}, 11, 2 );
-	}
+	//}
 
 	add_action( 'sp_pre_footer', function(){
 
