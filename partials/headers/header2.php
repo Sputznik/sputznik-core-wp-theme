9f1ada2b346bb3e5f51e113f0ebf5d06cd0fd2a4
unknown <samvthom16@gmail.com>
2019-07-15 15:09:25 +0530

    #22 - container class change from container-fluid

--- partials/headers/header2.php
+++ partials/headers/header2.php
@@ -1,5 +1,5 @@
 <nav class="navbar header2">
-	<div class="container-fluid">
+	<div class="container">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle top-buffer" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span>
 				<span class="icon-bar"></span>
@@ -8,9 +8,9 @@
 			</button>
 			<?php do_action('sp_logo');?>
 		</div>
-		
+
 		<?php do_action('sp_nav_menu');?>
 
 	</div>
-	<!-- /.container-fluid --> 
+	<!-- /.container-fluid -->
 </nav>
