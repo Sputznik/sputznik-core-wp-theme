1d5055e1743693750fa47c752916172b9817b47b
unknown <samvthom16@gmail.com>
2019-07-24 10:55:24 +0530

    #23 - total number of search results display in the search template

--- search.php
+++ search.php
@@ -1,20 +1,18 @@
 <?php get_header();?>
+<?php global $wp_query;?>
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12 search-content">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s' ), get_search_query() ); ?></h1>
-					
-					<?php
-					if (have_posts()) : 
-						while ( have_posts() ) : 
-							the_post(); ?> 
-							<div>
-								<h2 class='orbit-title'><a href='<?php the_permalink();?>'><?php the_title();?></a></h2>
-								<div class='orbit-excerpt'><?php the_excerpt();?></div>
-							</div>
-							<a class='orbit-btn' href='<?php the_permalink();?>'>Continue Reading</a> <?php
-						endwhile;
-					
+				<h1 class="page-title"><?php printf( __( '(%d) Search Results for: %s' ), $wp_query->found_posts, get_search_query() ); ?></h1>
+				<hr>
+				<?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+				<div style="margin-bottom: 30px;">
+					<h2 class='orbit-title'><a href='<?php the_permalink();?>'><?php the_title();?></a></h2>
+					<div class='orbit-excerpt'><?php the_excerpt();?></div>
+					<a class='orbit-btn' href='<?php the_permalink();?>'>Continue Reading</a>
+				</div>
+				<?php endwhile;?>
+				<?php
 						// Previous/next page navigation.
 						_e('<div>');
 						the_posts_pagination(
@@ -28,7 +26,7 @@
 						_e('</div>');
 
 			 		else :
-			 			printf( __('Sorry, but nothing matched your search terms. Please try again with some different keywords.') ); 	
+			 			printf( __('Sorry, but nothing matched your search terms. Please try again with some different keywords.') );
 
 			 	endif; ?>
 			</div>
