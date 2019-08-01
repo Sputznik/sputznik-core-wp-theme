d5691f274c5ee329938cda4692b5d02a64088b03
unknown <samvthom16@gmail.com>
2019-08-01 21:57:00 +0530

    Update post common template

--- partials/post-common.php
+++ partials/post-common.php
@@ -5,9 +5,10 @@
 
 <div class='orbit-thumbnail-bg' style='background-image: url( "<?php _e( $thumbnail[0] );?> ");position: relative;'>
 	<a href='<?php the_permalink();?>' style="position: absolute; top:0;left:0;width:100%;height: 100%;"></a>
-</div>					
-<div>
-	<h1 class='orbit-title'><?php the_title();?></h1>
+</div>
+<div class="orbit-content">
+	<h1 class='orbit-title'><a href='<?php the_permalink();?>'><?php the_title();?></a></h1>
+	<hr>
 	<div class='orbit-excerpt'><?php the_excerpt();?></div>
 </div>
 <a class='orbit-btn' href='<?php the_permalink();?>'>Continue Reading</a>
