cc8d86f01df5327d41311681cd8d2a59675f3352
unknown <samvthom16@gmail.com>
2019-08-07 10:24:19 +0530

    #13 - add resource template to single post

--- lib/customize-theme/post-single.php
+++ lib/customize-theme/post-single.php
@@ -5,16 +5,14 @@ add_action('customize_register', function( $wp_customize ){
 
   $sp_customize->section( $wp_customize, 'sp_theme_panel', 'sp_single_post_section', 'Single Post Section', 'Change Template For Single Post');
 
-
-
   /** SINGLE POST TYPE TEMPLATES */
   $templates_arr = apply_filters( 'sp_single_template_options', array(
     'single1' => 'Default',
     'single2' => 'Overlay featured image',
     'single3' => 'Blog Post',
-    //'template4'	=> 'Template 4'
+    'single4'	=> 'Resource'
   ) );
 
-    $sp_customize->dropdown( $wp_customize, 'sp_single_post_section', '[single_template]', 'Template Type', 'template1', $templates_arr);
+  $sp_customize->dropdown( $wp_customize, 'sp_single_post_section', '[single_template]', 'Template Type', 'template1', $templates_arr);
 
 });
