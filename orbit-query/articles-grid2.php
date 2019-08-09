dc016befa20224be2897bf7c71fa0b7605fa0c1a
Steve <anil.stephen66@gmail.com>
2019-08-09 13:40:15 +0530

    Added two-grid template

---
+++ orbit-query/articles-grid2.php
@@ -0,0 +1,7 @@
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="orbit-two-grid">
+	<?php while( $this->query->have_posts() ) : $this->query->the_post();?>
+	<li class="orbit-article-db orbit-list-db">
+		<?php get_template_part('partials/post', 'common');?>
+	</li>
+	<?php endwhile;?>
+</ul>
