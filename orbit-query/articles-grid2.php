f27a9fb215e3f0c30d3e1e0d16ba025501f29545
unknown <samvthom16@gmail.com>
2019-08-23 18:02:15 +0530

    Bugfix on orbit grid2

--- orbit-query/articles-grid2.php
+++ orbit-query/articles-grid2.php
@@ -1,4 +1,4 @@
-<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="orbit-two-grid">
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article-db');?>" data-url="<?php _e( $atts['url'] );?>" class="orbit-two-grid">
 	<?php while( $this->query->have_posts() ) : $this->query->the_post();?>
 	<li class="orbit-article-db orbit-list-db">
 		<?php get_template_part('partials/post', 'common');?>
