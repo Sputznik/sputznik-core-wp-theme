03e6975cec1b1c46bbf45f96d1735ab2ec6d1396
unknown <samvthom16@gmail.com>
2019-09-09 20:09:32 +0530

    Refactor

--- partials/singles/single2.php
+++ partials/singles/single2.php
@@ -20,26 +20,8 @@
       </div>
     </div>
     <div class="space">
-      <div class="entry-comments">
-      <?php
-        // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) {
-          comments_template();
-        }
-      ?>
-      </div>
-      <div class="entry-post-nav">
-        <?php the_post_navigation(
-          array(
-            'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-              '<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-              '<span class="post-title">%title</span>',
-            'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-              '<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-              '<span class="post-title">%title</span>',
-          )
-        ); ?>
-      </div>
+      <?php get_template_part( 'partials/comments', 'box');?>
+      <?php get_template_part( 'partials/post', 'navigation');?>
     </div>
   </article>
 <?php endwhile;endif;?>
