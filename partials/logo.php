c57771230aeb6ca17a6e2e2067f8bd7320ac4c85
unknown <samvthom16@gmail.com>
2020-01-30 16:54:08 +0530

    #33 - alt text as tagline

--- partials/logo.php
+++ partials/logo.php
@@ -1,17 +1,17 @@
 <?php
 	global $sp_customize;
-		
+
 	$option = $sp_customize->get_option();
 
 	$nav_transparent = sp_is_sticky_nav_transparent();
-	
+
 	if( isset( $option['logo'] ) && $option['logo'] ){
-			
-		$img_alt = esc_attr( get_bloginfo( 'name', 'display' ) );
-			
+
+		$img_alt = esc_attr( get_bloginfo( 'description', 'display' ) );
+
 		/* CONTAINER */
 		_e('<div class="logo logo-normal"><a class="navbar-brand" href="'.get_bloginfo('url').'">');
-		
+
 		/*DESKTOP LOGO*/
 		if( isset( $option['logo']['desktop'] ) && $option['logo']['desktop'] ){
 
@@ -24,15 +24,15 @@
 
 		/* MOBILE LOGO */
 	 	if( isset( $option['logo']['mobile'] ) && $option['logo']['mobile'] ){
-	 				
+
 	 		$img_mobile_class = 'visible-xs';
-	 				
+
 	 		$img_mobile_src = ( isset( $option['logo']['mobile'] ) && $option['logo']['mobile'] ) ? esc_url( $option['logo']['mobile'] ) : esc_url( $option['logo']['desktop'] );
-	 				
+
 	 		_e('<img class="'.$img_mobile_class.'" src="'.$img_mobile_src.'" alt="'.$img_alt.'">');
-	 				
-	 	} 
-		
+
+	 	}
+
 		// END OF CONTAINER
 		_e('</a></div>');
 	}
