f45f97b9e679317b6fed598f5b3e02c1303d346f
psychome004 <anil.stephen66@gmail.com>
2021-02-03 11:44:19 +0530

    Updated youtube-video-popup template

--- partials/youtube-video-popup.php
+++ partials/youtube-video-popup.php
@@ -5,7 +5,7 @@ $embed_url = $this->get_youtube_link( $url  );
 $thumbnail = $this->get_video_thumbnail( $url, $resolution );
 ?>
 <div class="sp-youtube-frame" style="background-image: url(<?php _e( $thumbnail );?>);height: <?php _e($height);?>">
-  <a class="play-btn" href="#sp-youtube-modal" data-toggle="modal" data-behaviour="sp-youtube" data-url="<?php _e( $embed_url );?>">
+  <a class="play-btn" href="#sp-youtube-modal" data-toggle="modal" data-behaviour="sp-youtube" data-url="<?php _e( $embed_url );?>" aria-label="Play Button">
     <div class="overlay"></div>
   </a>
 </div>
