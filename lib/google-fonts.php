9b2aee3fcf9ddf3be450ef945684ebc9f1f8f4aa
psychome004 <anil.stephen66@gmail.com>
2022-04-11 12:31:56 +0530

    Added google-font Rubik

--- lib/google-fonts.php
+++ lib/google-fonts.php
@@ -141,6 +141,12 @@
 					'url'	=> 'Playfair+Display'
 				);
 
+			$fonts[] = array(
+  			'slug'	=> 'rubik',
+  			'name'	=> 'Rubik',
+	  		'url'		=> 'Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900'
+  		);
+
 		return $fonts;
 
 	});
