9390dedece14c5e5a00f0153b5fc26271f05df1f
psychome004 <anil.stephen66@gmail.com>
2022-10-25 14:41:36 +0530

    Added google-font Karla

--- lib/google-fonts.php
+++ lib/google-fonts.php
@@ -135,17 +135,23 @@
   			'url'	=> 'Maven+Pro:400,500,700,900'
   		);
 
-			$fonts[] = array(
-					'slug'	=> 'playfair',
-					'name'	=> 'Playfair Display',
-					'url'	=> 'Playfair+Display'
-				);
+		$fonts[] = array(
+				'slug'	=> 'playfair',
+				'name'	=> 'Playfair Display',
+				'url'	=> 'Playfair+Display'
+			);
 
-			$fonts[] = array(
-  			'slug'	=> 'rubik',
-  			'name'	=> 'Rubik',
-	  		'url'		=> 'Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900'
-  		);
+		$fonts[] = array(
+			'slug'	=> 'rubik',
+			'name'	=> 'Rubik',
+  		'url'		=> 'Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900'
+		);
+
+		$fonts[] = array(
+			'slug'	=> 'karla',
+			'name'	=> 'Karla',
+  		'url'		=> 'Karla:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800'
+		);
 
 		return $fonts;
 
