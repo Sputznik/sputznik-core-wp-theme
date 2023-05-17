5705410d8c97f920fa8fa53730c70080dd2b99e1
psychome004 <anil.stephen66@gmail.com>
2023-05-17 14:02:06 +0530

    Add google fonts Sora, Inter

--- lib/google-fonts.php
+++ lib/google-fonts.php
@@ -153,6 +153,18 @@
   		'url'		=> 'Karla:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800'
 		);
 
+		$fonts[] = array(
+			'slug'	=> 'sora',
+			'name'	=> 'Sora',
+			'url'		=> 'Sora:wght@100;200;300;400;500;600;700;800'
+		);
+
+		$fonts[] = array(
+			'slug'	=> 'inter',
+			'name'	=> 'Inter',
+			'url'		=> 'Inter:wght@100;200;300;400;500;600;700;800;900'
+		);
+
 		return $fonts;
 
 	});
